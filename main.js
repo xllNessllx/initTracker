21e6c008d799b61600c5ef32b7b28cff9dbfd744
Sebastian Markus <sebi.markus@googlemail.com>
2021-09-13 09:21:55 +0200

    added files

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      //frame: false
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.removeMenu()
+
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+  
