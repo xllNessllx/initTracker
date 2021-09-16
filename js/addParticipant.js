d05ba86c0aecc9732c27f4219345e2d87f7b8f2d
Sebastian Markus <sebi.markus@googlemail.com>
2021-09-16 18:31:09 +0200

    check for invalid input + clear input fields after adding a perticipant

--- js/addParticipant.js
+++ js/addParticipant.js
@@ -10,9 +10,29 @@ function compareInit(a,b){
 }
 
 function addParticipant(){
+    // get values
     var new_name = document.getElementById('name_id').value
     var new_initiative = document.getElementById('initiative_id').value
-    participant = {initiative: new_initiative, name: new_name}
+    var new_hp = document.getElementById('hp_id').value
+
+    // check values
+    if (new_name == ""){
+        return
+    }
+    if(new_initiative == "" || new_initiative < 0){
+        return
+    }
+    if(new_hp == "" || new_hp < 0){
+        return
+    }
+
+    // add participant
+    participant = {initiative: new_initiative, name: new_name, health: new_hp}
     listOfParticipants.push(participant)
     listOfParticipants.sort(compareInit)
+
+    // clear input fields
+    document.getElementById('name_id').value = ""
+    document.getElementById('initiative_id').value = ""
+    document.getElementById('hp_id').value = ""
   }
